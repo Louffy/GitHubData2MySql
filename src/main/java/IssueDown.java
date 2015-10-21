bbefc1ae05fb9d0fa80975276b3a1f9ad61adf1f
louffy <zfx0906@gmail.com>
2015-10-21 18:25:00 +0800

    1.0

--- src/main/java/IssueDown.java
+++ src/main/java/IssueDown.java
@@ -16,54 +16,64 @@ public class IssueDown {
     private String uriBase;
     public static final String clientId = "6701d27a7747d3a68f04";
     public static final String clientSecret= "0de67dc4a9fed164135cbe1266b72c3779df359d";
-    public static final String end = "?client_id=6701d27a7747d3a68f04&client_secret=0de67dc4a9fed164135cbe1266b72c3779df359d";
-    public IssueDown(String s){
+    public String end;//?client_id=6701d27a7747d3a68f04&client_secret=0de67dc4a9fed164135cbe1266b72c3779df359d
+    //?client_id=9e08256d21154103bd05&client_secret=6c12e87337f2054be616401d9128ba40ebc03e8b
+    public IssueDown(String s,String x){
         uriBase = s;
         issues = new TreeMap<String, String>();
         comments = new TreeMap<String, String>();
         events = new TreeMap<String, String>();
         ids = new ArrayList<String>();
         mis = new ArrayList<String>();
+        end = new String(x);
     }
     public void downById(String id){
-        Client c = new Client(new StringBuffer(uriBase+"/"+id+IssueDown.end));
-        if(c.getReposeCode()==200){
-            String issue = downIssue(id);
+
+        String issue = downIssue(id);
+        if(issue==null) {
+            mis.add(id);
+            System.out.println("mis " + id);
+        }
+        else{
             String comment = downComment(id);
             String event = downEvent(id);
             issues.put(id,issue);
             comments.put(id,comment);
-            events.put(id,event);
+            events.put(id, event);
             ids.add(id);
             System.out.println(id);
         }
-        else
-            mis.add(id);
+
+
     }
     public String downIssue(String id){
-        Client c = new Client(new StringBuffer(uriBase+"/"+id +IssueDown.end));
+        Client c = new Client(new StringBuffer(uriBase+"/"+id +end));
+        System.out.println(uriBase+"/"+id +end);
         try {
-            return c.getData();
+
+                return c.getData();
         } catch (IOException e) {
-            e.printStackTrace();
+
         }
         return null;
     }
     public String downComment(String id){
-        Client c = new Client(new StringBuffer(uriBase+"/"+id+"/comments"+IssueDown.end));
+        Client c = new Client(new StringBuffer(uriBase+"/"+id+"/comments"+end));
         try {
-            return c.getData();
+
+                return c.getData();
         } catch (IOException e) {
-            e.printStackTrace();
+
         }
         return null;
     }
     public String downEvent(String id){
-        Client c = new Client(new StringBuffer(uriBase+"/"+id+"/events"+IssueDown.end));
+        Client c = new Client(new StringBuffer(uriBase+"/"+id+"/events"+end));
         try {
-            return c.getData();
+
+                return c.getData();
         } catch (IOException e) {
-            e.printStackTrace();
+
         }
         return null;
     }
@@ -76,10 +86,12 @@ public class IssueDown {
                 Map.Entry entry = (Map.Entry) it.next();
                 fo.write((String) entry.getKey());
                 fo.write(":");
-                fo.write((String) entry.getValue());
+                if(entry.getValue()!=null)
+                    fo.write((String) entry.getValue());
                 fo.write("\n");
             }
             fo.close();
+            data.clear();
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -98,6 +110,7 @@ public class IssueDown {
 
             }
             fo.close();
+            data.clear();
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -108,15 +121,27 @@ public class IssueDown {
         saveFile(file2,comments);
         saveFile(file3,events);
         saveFile(file4,ids);
+
     }
 
     public static void main(String[] args){
 
-        IssueDown issueDown = new IssueDown("https://api.github.com/repos/docker/docker/issues");
-        for(int i=100;i<10000;i++) {
+        IssueDown issueDown = new IssueDown("https://api.github.com/repos/docker/docker/issues",args[2].replaceAll("\"",""));
+        int c=0;
+        //System.out.println(args[1]);
+
+        int i=Integer.parseInt(args[0]);
+        for(;i<Integer.parseInt(args[1]);i++) {
             issueDown.downById(String.valueOf(i));
+            c++;
+            if(c>=200){
+                issueDown.saveAllFiles("./issues"+String.valueOf(i), "./comments"+String.valueOf(i),"./events"+String.valueOf(i),"./ids"+String.valueOf(i));
+                c=0;
+            }
+
         }
-        issueDown.saveAllFiles("./issues1", "./comments1","./events1","./ids1");
+        issueDown.saveAllFiles("./issues"+String.valueOf(i), "./comments"+String.valueOf(i),"./events"+String.valueOf(i),"./ids"+String.valueOf(i));
+
 
     }
 
